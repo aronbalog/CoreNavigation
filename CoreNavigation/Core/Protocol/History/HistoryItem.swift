ff55e3e44c7d8014ba672823faca6d7902f7cabd
Aron Balog <aronbalog@gmail.com>
2019-01-14 16:04:41 +0100

    Removed `weak` flag in protocol HistoryItem

--- CoreNavigation/Core/Protocol/History/HistoryItem.swift
+++ CoreNavigation/Core/Protocol/History/HistoryItem.swift
@@ -1,7 +1,7 @@
 import Foundation
 
-protocol HistoryItem {
-    weak var viewController: UIViewController? { get }
+protocol HistoryItem: class {
+    var viewController: UIViewController? { get }
     var navigationType: NavigationType { get }
     func go(_ direction: HistoryDirection, animated: Bool, completion: (() -> Void)?)
 }
