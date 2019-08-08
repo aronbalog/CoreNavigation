bb24e77dabe3cb2ac66e0f0997aedcab43a92ebd
Aron Balog <aronbalog@gmail.com>
2019-08-08 13:53:01 +0200

    Initial Commit

---
+++ CoreNavigation/CoreNavigation.h
@@ -0,0 +1,19 @@
+//
+//  CoreNavigation.h
+//  CoreNavigation
+//
+//  Created by Aron Balog on 08/08/2019.
+//  Copyright © 2019 Forensic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreNavigation.
+FOUNDATION_EXPORT double CoreNavigationVersionNumber;
+
+//! Project version string for CoreNavigation.
+FOUNDATION_EXPORT const unsigned char CoreNavigationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreNavigation/PublicHeader.h>
+
+
