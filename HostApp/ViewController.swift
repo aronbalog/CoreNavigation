c40d251a004bbb9d2addcd33e60696f648d23939
Aron Balog <aronbalog@gmail.com>
2019-09-23 14:44:48 +0200

    Minor changes

--- HostApp/ViewController.swift
+++ HostApp/ViewController.swift
@@ -1,73 +1,6 @@
 import UIKit
 import CoreNavigation
 
-struct VC2Dest: Destination, Protectable {
-    typealias ViewControllerType = ViewController2
-    
-    func protect(with context: Protection.Context) throws {
-        enum MyError: Error {
-            case unknown
-        }
-        context.allow()//.disallow(with: MyError.unknown)
-    }
-}
-
-struct VC3Dest: Destination, Routable {
-    let id: String!
-    
-    init(parameters: [String : Any]?) {
-        self.id = parameters?["id"] as? String
-    }
-    
-    typealias ViewControllerType = ViewController3
-    
-    static func routePatterns() -> [String] {
-        return ["some/:id"]
-    }
-    
-    func resolve(with resolver: Resolver<VC3Dest>) {
-        resolver.complete(viewController: ViewController3.init(id: id))
-    }
-}
-
-class ViewController2: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        title = "Title"
-        view.backgroundColor = .white
-        navigationItem.setRightBarButton(UIBarButtonItem.init(barButtonSystemItem: UIBarButtonItem.SystemItem.stop, target: self, action: #selector(didTap)), animated: false)
-    }
-        
-    @objc func didTap() {
-        Dismiss()
-    }
-}
-
-class ViewController3: UIViewController {
-    init(id: String) {
-        super.init(nibName: nil, bundle: nil)
-        self.title = id
-    }
-    
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .orange
-        navigationItem.setRightBarButton(UIBarButtonItem.init(barButtonSystemItem: UIBarButtonItem.SystemItem.stop, target: self, action: #selector(didTap)), animated: false)
-    }
-    
-    @objc func didTap() {
-        Dismiss()
-    }
-}
-
 class ViewController: UIViewController {
     lazy var button: UIButton = {
         let button = UIButton(type: .system)
@@ -81,8 +14,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        Register(VC3Dest.self)
         
         view.backgroundColor = .orange
         
@@ -105,13 +36,11 @@ class ViewController: UIViewController {
             .animated(false)
             .embed(inside:
                 .navigationController(UINavigationController.self, { () -> Embedding.EmbeddingType in
-                    .pageViewController(UIPageViewController.self, {
-                        .none
-                    })
+                    .none
                 })
             )
         }
-//        ==> "some/1".push(animated: false)
+        ==> "some/2".push(animated: false)
 //        ==> "some/2".push(animated: false)
 //        ==> "some/3".push(animated: false)
 //        ==> "some/4".push(animated: false)
